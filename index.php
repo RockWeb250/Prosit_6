05257103e13a6d24f5195a51c49528343bad379b
RockWeb250 <zoe.truongcqnh@viacesi.fr>
2025-03-23 18:26:36 +0100

    test vm

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
  * It handles the routing and dispatches requests to the appropriate controller methods.
  */
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "index.php fonctionne !";
+
 require "vendor/autoload.php";
 
 use App\Controllers\OfferController;
